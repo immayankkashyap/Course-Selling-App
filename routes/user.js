b94b6762b1132662bd11fc289a8d7bfd99520405
Mayank <immayankkashyap@gmail.com>
2025-07-28 16:46:35 +0000

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const {app} = require('express')
+
+app.post('/signup',async(req,res)=>{
+try{
+    const email = req.body.email;
+    
+}
+})
+
+app.post('/login',async(req,res)=>{
+
+})
+
+app.post('/purchase',async(req,res)=>{
+
+})
+
+app.post('/mycourses',async(req,res)=>{
+
+})
